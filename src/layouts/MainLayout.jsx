b5f26b694d81f04b92bd12e4ee885bb79e9d44bc
MdAshiqurRahman98 <MDASHIQUROFFICIAL@gmail.com>
2023-10-07 16:45:07 +0600

    Navbar is added

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -5,7 +5,7 @@ import Footer from "../pages/Shared/Footer/Footer";
 
 const MainLayout = () => {
     return (
-        <div>
+        <div className="max-w-screen-xl mx-auto px-8 md:px-16 lg:px-24 py-5 mb-11 mt-1">
             <Navbar></Navbar>
             <Outlet></Outlet>
             <Footer></Footer>
--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -1,9 +1,88 @@
+import { NavLink } from "react-router-dom";
 
 
 const Navbar = () => {
     return (
-        <div>
-            Navbar
+        <div className="">
+            <nav className="flex justify-between py-5 mb-24 items-center">
+                <figure><img className="w-[79%]" src="/public/Resources/Logo.png" alt="" /></figure>
+                <ul className="flex gap-5">
+                    <li className="text-lg">
+                        <NavLink
+                            to="/"
+                            className={({ isActive, isPending }) =>
+                                isPending
+                                    ? "pending"
+                                    : isActive
+                                        ? "text-[#FF444A] text-lg font-bold"
+                                        : ""
+                            }
+                        >
+                            Home
+                        </NavLink>
+                    </li>
+
+                    <li className="text-lg">
+                        <NavLink
+                            to="/eventCart"
+                            className={({ isActive, isPending }) =>
+                                isPending
+                                    ? "pending"
+                                    : isActive
+                                        ? "text-[#FF444A] text-lg font-bold"
+                                        : ""
+                            }
+                        >
+                            Event Cart
+                        </NavLink>
+                    </li>
+
+                    <li className="text-lg">
+                        <NavLink
+                            to="/dashboard"
+                            className={({ isActive, isPending }) =>
+                                isPending
+                                    ? "pending"
+                                    : isActive
+                                        ? "text-[#FF444A] text-lg font-bold"
+                                        : ""
+                            }
+                        >
+                            Dashboard
+                        </NavLink>
+                    </li>
+
+                    <li className="text-lg">
+                        <NavLink
+                            to="/login"
+                            className={({ isActive, isPending }) =>
+                                isPending
+                                    ? "pending"
+                                    : isActive
+                                        ? "text-[#FF444A] text-lg font-bold"
+                                        : ""
+                            }
+                        >
+                            Login
+                        </NavLink>
+                    </li>
+
+                    <li className="text-lg">
+                        <NavLink
+                            to="/register"
+                            className={({ isActive, isPending }) =>
+                                isPending
+                                    ? "pending"
+                                    : isActive
+                                        ? "text-[#FF444A] text-lg font-bold"
+                                        : ""
+                            }
+                        >
+                            Register
+                        </NavLink>
+                    </li>
+                </ul>
+            </nav>
         </div>
     );
 };
