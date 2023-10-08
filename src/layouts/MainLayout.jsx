882ee91abfbdaa1e9b13a2b6fe6fb3dfae1d4430
MdAshiqurRahman98 <MDASHIQUROFFICIAL@gmail.com>
2023-10-08 14:32:32 +0600

    footer is added

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -5,10 +5,16 @@ import Footer from "../pages/Shared/Footer/Footer";
 
 const MainLayout = () => {
     return (
-        <div className="max-w-screen-xl mx-auto px-7 md:px-16 lg:px-16 py-3 mb-11 mt-1">
-            <Navbar></Navbar>
-            <Outlet></Outlet>
-            <Footer></Footer>
+        <div>
+            <div className="max-w-screen-xl mx-auto px-7 md:px-16 lg:px-16 py-3 mt-1 mb-7">
+                <Navbar></Navbar>
+                <Outlet></Outlet>
+            </div>
+            <div className="bg-black">
+                <div className="max-w-screen-xl mx-auto px-7 md:px-16 lg:px-9 py-3">
+                    <Footer></Footer>
+                </div>
+            </div>
         </div>
     );
 };
--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -2,9 +2,29 @@
 
 const Footer = () => {
     return (
-        <div>
-            Footer
-        </div>
+        <footer className="footer p-10 text-white">
+            <aside className="mt-10">
+                <p>Team Event Company Ltd.<br />Copyright &copy; Team Event | All rights reserved.</p>
+            </aside>
+            <nav>
+                <header className="footer-title">Services</header>
+                <a className="link link-hover">Workshops and webinars</a>
+                <a className="link link-hover">Educational conferences</a>
+                <a className="link link-hover">Training programs</a>
+            </nav>
+            <nav>
+                <header className="footer-title">Company</header>
+                <a className="link link-hover">Mission and vision</a>
+                <a className="link link-hover">Contact</a>
+                <a className="link link-hover">Career</a>
+            </nav>
+            <nav>
+                <header className="footer-title">Legal</header>
+                <a className="link link-hover">Terms of use</a>
+                <a className="link link-hover">Privacy policy</a>
+                <a className="link link-hover">Cookie policy</a>
+            </nav>
+        </footer>
     );
 };
 
