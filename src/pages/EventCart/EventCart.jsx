8100d5ac61ca9b40dc883e91fbfab30341a32b28
MdAshiqurRahman98 <MDASHIQUROFFICIAL@gmail.com>
2023-10-08 17:22:23 +0600

    EventCart is updated

--- src/pages/EventCart/EventCart.jsx
+++ src/pages/EventCart/EventCart.jsx
@@ -1,14 +1,11 @@
 import { useEffect, useState } from "react";
 import { Link, useLoaderData } from "react-router-dom";
-
+import { getStoredService } from "../../utilities/localStorage";
 
 const EventCart = () => {
     const services = useLoaderData();
 
     const [displayServices, setDisplayServices] = useState([]);
-
-    const [serves, setServes] = useState([]);
-    const [dataLength, setDataLength] = useState(4);
 
     useEffect(() => {
         const storedServiceIds = getStoredService();
@@ -26,17 +23,11 @@ const EventCart = () => {
         }
     }, [services]);
 
-    useEffect(() => {
-        fetch('services.json')
-            .then(res => res.json())
-            .then(data => setServes(data));
-    }, []);
-
     return (
         <>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-9 -mt-14">
                 {
-                    displayServices.slice(0, dataLength).map(service => <div key={service.id} className="card rounded-lg p-5 bg-[#F8F8F8] h-96">
+                    displayServices.map(service => <div key={service.id} className="card rounded-lg p-5 bg-[#F8F8F8] h-96">
                         <div className="mb-1 flex flex-grow justify-center">
                             <img className="w-40 h-36" src={service.image} alt="" />
                         </div>
@@ -51,15 +42,6 @@ const EventCart = () => {
                     </div>)
                 }
             </div>
-            {
-                serves && <div className={serves.length < 5 ? 'hidden' : ''}>
-                    <div className="text-center mt-5">
-                        <div className={dataLength === serves.length ? 'hidden' : ''}>
-                            <button onClick={() => setDataLength(serves.length)} className="btn text-white bg-[#009444] hover:bg-[#009444] normal-case text-sm font-semibold rounded-lg mt-3 w-24">See All</button>
-                        </div>
-                    </div>
-                </div>
-            }
         </>
     );
 };
