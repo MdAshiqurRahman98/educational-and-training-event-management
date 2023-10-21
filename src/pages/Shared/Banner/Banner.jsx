4c4501a54951e781b6937ab50f0c7b514bf90d8e
MdAshiqurRahman98 <MDASHIQUROFFICIAL@gmail.com>
2023-10-21 20:11:12 +0600

    home page updated

--- src/pages/Shared/Banner/Banner.jsx
+++ src/pages/Shared/Banner/Banner.jsx
@@ -2,7 +2,7 @@
 
 const Banner = () => {
     return (
-        <div className="flex justify-evenly items-center">
+        <div className="lg:flex justify-evenly items-center mb-14 lg:mb-1">
             <h1 className="flex-1 text-5xl font-bold text-left ml-5">Educational and Training Event Management</h1>
             <figure className="flex-1 -mr-36"><img className="w-[500px]" src="https://i.ibb.co/sqGzrrM/logo.png" alt="" /></figure>
         </div>
